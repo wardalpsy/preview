dabf250a02e738879f2b80386906b134654874c6
oxypteros <oxypteros@gmail.com>
2026-04-23 14:19:57 +0200

    revert changes

--- src/routes/[[lang=lang]]/+page.svelte
+++ src/routes/[[lang=lang]]/+page.svelte
@@ -6,6 +6,7 @@
 	//import Approach from '$lib/components/home/Approach.svelte';
 	import Approach2 from '$lib/components/home/Approach2.svelte';
 	//import Approach3 from '$lib/components/home/Approach3.svelte';
+	import Services from '$lib/components/home/Services.svelte';
 	import MidHero from '$lib/components/home/MidHero.svelte';
 	import FAQ from '$lib/components/home/FAQ.svelte';
 	import Articles from '$lib/components/home/Articles.svelte';
@@ -17,6 +18,7 @@
 <Hero />
 <About about={data.about} content={data.aboutContent} />
 <Approach2 />
+<Services />
 {#if i18n.t.articles.enabled}
 	<Articles articles={data.articles} />
 {/if}
